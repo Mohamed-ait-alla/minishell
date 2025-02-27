de3cbc15e86cf8b393de5fb5418b1b1b37f2cd42
Mohamed Dahani <mdahani@e2r10p11.1337.ma>
2025-02-27 19:25:15 +0000

    add a enum & some structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:42:01 by mdahani           #+#    #+#             */
-/*   Updated: 2025/02/26 17:33:32 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/02/27 17:37:21 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_IN,
+	TOKEN_REDIRECT_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_SEMICOLON,
+}						t_token_type;
+
+typedef struct s_token
+{
+	t_token_type		type;
+	char				*value;
+	struct s_token		*next;
+}						t_token;
+
+typedef struct s_commands
+{
+	char				**args;
+	char				*input_file;
+	char				*output_file;
+	int					append;
+	struct s_commands	*next;
+}						t_commands;
 
 #endif
