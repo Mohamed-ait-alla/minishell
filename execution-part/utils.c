f25fa5a5407b45c1f8190f5477e2fb519b7cbfb6
Mohamed Ait Alla <mohamedmhdo2000@gmail.com>
2025-04-19 11:35:36 +0100

    Update execution-part: Implement cd builtin command

--- execution-part/utils.c
+++ execution-part/utils.c
@@ -6,7 +6,7 @@
 /*   By: mait-all <mait-all@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 10:37:40 by mait-all          #+#    #+#             */
-/*   Updated: 2025/04/13 11:51:21 by mait-all         ###   ########.fr       */
+/*   Updated: 2025/04/18 22:17:46 by mait-all         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,33 @@ int	is_builtin(char *cmd)
 			ft_strcmp(cmd, "unset") == 0 ||
 			ft_strcmp(cmd, "env") == 0 ||
 			ft_strcmp(cmd, "exit") == 0);
+}
+
+char	**copy_env(char **envp)
+{
+	char	**new_env;
+	int		i;
+
+	i = 0;
+	while (envp && envp[i])
+		i++;
+	new_env = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!new_env)
+	{
+		perror("malloc: ");
+		return (NULL);
+	}
+	i = 0;
+	while (envp && envp[i])
+	{
+		new_env[i] = ft_strdup(envp[i]);
+		if (!new_env[i])
+		{
+			perror("ft_strdup: ");
+			return (NULL);
+		}
+		i++;
+	}
+	new_env[i] = NULL;
+	return (new_env);
 }
