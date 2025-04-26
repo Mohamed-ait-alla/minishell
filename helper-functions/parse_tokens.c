a28388408118ffc320a9a3952cdfb66f92624592
Mohamed Dahani <mdahani@e2r1p11.1337.ma>
2025-04-26 12:04:37 +0100

    add condition of here doc from tokens into commands

--- helper-functions/parse_tokens.c
+++ helper-functions/parse_tokens.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:34:29 by mdahani           #+#    #+#             */
-/*   Updated: 2025/04/26 12:02:36 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/04/26 12:04:31 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,5 @@ t_commands	*parse_tokens(t_token *tokens)
 		}
 		tokens = tokens->next;
 	}
-	printf("append: %d\n", current_cmd->append);
-	printf("heredoc: %d\n", current_cmd->heredoc);
 	return (cmd_list);
 }
